ebc6d881c1c6a97ae4b6e56741c7e3902fb2b634
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-06-26 23:38:42 +0530

    Add files via upload

---
+++ uc1.java
@@ -0,0 +1,8 @@
+package SnakeAndLadder;
+
+public class uc1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Game");
+        int startPosition =0;
+    }
+}
